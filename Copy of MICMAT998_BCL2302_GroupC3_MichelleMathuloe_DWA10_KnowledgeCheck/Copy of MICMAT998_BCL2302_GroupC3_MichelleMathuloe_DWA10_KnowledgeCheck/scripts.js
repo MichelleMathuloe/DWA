1fc83bfddd1565b66bbd713a2a7546305bddf39b
MichelleMathuloe <124686615+MichelleMathuloe@users.noreply.github.com>
2023-06-22 15:31:55 +0200

    EDITED DWA10

---
+++ Copy of MICMAT998_BCL2302_GroupC3_MichelleMathuloe_DWA10_KnowledgeCheck/Copy of MICMAT998_BCL2302_GroupC3_MichelleMathuloe_DWA10_KnowledgeCheck/scripts.js
@@ -0,0 +1,52 @@
+const MAX_NUMBER = 15;
+const MIN_NUMBER = -5;
+const STEP_AMOUNT = 1;
+
+
+const number = document.querySelector('[data-key="number"]')
+const subtract = document.querySelector('[data-key="subtract"]')
+const add = document.querySelector('[data-key="add"]')
+const reset = document.querySelector('[data-key="reset"]');
+
+const subtractHandler = () => {
+    const newValue = parseInt(number.value) - STEP_AMOUNT;
+   number.value = newValue;
+
+   if (add.disabled === true) {
+    add.disabled = false;
+   }
+
+   if (newValue <= MIN_NUMBER) {
+    subtract.disabled = true;
+   }
+};
+
+const addHandler = () => {
+    const newValue = parseInt(number.value) + STEP_AMOUNT;
+    number.value = newValue;
+
+    if (subtract.disabled === true) {
+        subtract.disabled = false;
+    }
+
+    if(newValue >=MAX_NUMBER) {
+        add.disabled = true;
+        }
+       
+};
+//added a handleReset button which is functonal and is able to reset tally count to 0
+const handleReset = () => {
+    number.value = 0;
+    subtract.disabled = true;
+    add.disabled = false;
+    //added alert when reset button has been clicked
+    window.alert("Counter has been reset")
+};
+
+
+subtract.addEventListener('click', subtractHandler);
+add.addEventListener('click',  addHandler);
+reset.addEventListener('click', handleReset);
+
+
+
